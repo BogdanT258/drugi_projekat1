3d3267498b5422afa69215ef21c2c0355f2921a8
Bogdan Trivic <62573562+BogdanT258@users.noreply.github.com>
2022-03-11 13:42:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Card from "./Card.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      data: []
+    }    
+  }
+  componentDidMount = async () =>{
+    const res = await(await fetch('http://localhost:3004/posts')).json();
+    console.log(res);    
+    this.setState({
+      data: res
+    })
+  }
+
+  render(){
+    return (
+      <div>        
+        {this.state.data.map(item => {
+          <Card key={item.id} object={item}/>
+        })}
+      </div>      
+    )
+  }
 }
 
 export default App;
